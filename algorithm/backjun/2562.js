c95326a7ae02e3e9da6ba854685f489816b92ea3
alpaca623 <alpaca623@gmail.com>
2019-07-03 16:02:34 +0900

    [adding] : 1149

---
+++ algorithm/backjun/2562.js
@@ -0,0 +1,19 @@
+const maximumCheck = (x) =>{
+  const result = 0;
+  let count = 1;
+  if(x > result){
+    result = x;
+    count++;
+  }
+  console.log(result);
+  console.log(count);
+}
+
+maximumCheck(3);
+maximumCheck(29);
+maximumCheck(38);
+maximumCheck(12);
+maximumCheck(57);
+maximumCheck(74);
+maximumCheck(85);
+maximumCheck(61);
